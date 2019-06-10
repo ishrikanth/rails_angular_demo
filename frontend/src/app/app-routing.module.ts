0944fa0cf6c1f6f5b825d1f436bee262a246c08c
Shrikanth <shrikanth.hathwar@ericsson.com>
2019-06-10 19:39:40 +0530

    Adding signin and signup option

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SignupComponent} from "./signup/signup.component";
+import {SigninComponent} from "./signin/signin.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'signup', component: SignupComponent},
+  {path: 'signin', component: SigninComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
